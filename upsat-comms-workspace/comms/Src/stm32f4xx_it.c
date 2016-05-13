c220e3bada76b49d50017c20f5e0281c74c29fa3
nchronas <nchronas@gmail.com>
2016-05-13 11:05:48 -0700

    integrated ecss services

--- upsat-comms-workspace/comms/Src/stm32f4xx_it.c
+++ upsat-comms-workspace/comms/Src/stm32f4xx_it.c
@@ -227,7 +227,7 @@ void DMA1_Stream7_IRQHandler(void)
 void UART5_IRQHandler(void)
 {
   /* USER CODE BEGIN UART5_IRQn 0 */
-
+  HAL_COMMS_UART_IRQHandler(&huart5);
   /* USER CODE END UART5_IRQn 0 */
   HAL_UART_IRQHandler(&huart5);
   /* USER CODE BEGIN UART5_IRQn 1 */
